be3322474cf5ca14ae818309b87c8bdf87b1e0bf
Protryon <max.bruce12@gmail.com>
2023-07-24 14:12:36 -0700

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+pin_project_lite::pin_project! {
+    pub struct MaybeFuture<O, F: Future<Output=O>> {
+        #[pin]
+        inner: Option<F>,
+    }
+}
+
+impl<O, F: Future<Output = O>> MaybeFuture<O, F> {
+    pub fn new(inner: Option<F>) -> Self {
+        Self { inner }
+    }
+
+    pub fn into_inner(self) -> Option<F> {
+        self.inner
+    }
+
+    pub fn inner(&self) -> &Option<F> {
+        &self.inner
+    }
+
+    pub fn inner_mut(&mut self) -> &mut Option<F> {
+        &mut self.inner
+    }
+}
+
+impl<O, F: Future<Output = O>> Future for MaybeFuture<O, F> {
+    type Output = O;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        match this.inner.as_pin_mut() {
+            Some(inner) => inner.poll(cx),
+            None => Poll::Pending,
+        }
+    }
+}
